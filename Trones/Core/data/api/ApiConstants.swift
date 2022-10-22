c27312341c56b1fca9edd4188119b7f4b9f50b0c
diayan <diayans@outlook.com>
2022-10-22 10:02:54 +0000

    create url request

---
+++ Trones/Core/data/api/ApiConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ApiConstants.swift
+//  Trones
+//
+//  Created by diayan siat on 22/10/2022.
+//
+
+import Foundation
+
+enum APIConstants {
+  static let host = "anapioficeandfire.com"
+  static let grantType = "client_credentials"
+  static let clientId = ""
+  static let clientSecret = ""
+}
