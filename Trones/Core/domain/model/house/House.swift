8e10277c1b14f0241b703f02ed4ee60780a6bf29
diayan <diayans@outlook.com>
2022-10-22 08:20:54 +0000

    model data using api response && create mock data

---
+++ Trones/Core/domain/model/house/House.swift
@@ -0,0 +1,21 @@
+//
+//  House.swift
+//  Trones
+//
+//  Created by diayan siat on 22/10/2022.
+//
+
+import Foundation
+
+struct House: Codable {
+    let url: String?
+    let name, region, coatOfArms, words: String?
+    let titles, seats: [String?]?
+    let currentLord, heir, overlord: String?
+    let founded: String?
+    let founder: String?
+    let diedOut: String?
+    let ancestralWeapons: [String?]?
+    let cadetBranches, swornMembers: [String?]?
+}
+
---
+++ Trones/Core/domain/model/house/HousesMock.swift
@@ -0,0 +1,31 @@
+//
+//  HouseMock.swift
+//  Trones
+//
+//  Created by diayan siat on 22/10/2022.
+//
+
+import Foundation
+
+private struct HousesMock: Codable {
+    let houses: [House]
+}
+
+private func loadHouses() -> [House] {
+    //find and load data from a file called HousesMock with an extension json
+    guard let url = Bundle.main.url(
+        forResource: "HousesMock",
+        withExtension: "json"
+    ),
+    let data = try? Data(contentsOf: url) else { return [] }
+    
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase //convert keys stored as snake case into camel case
+    let jsonMock = try? decoder.decode(HousesMock.self, from: data)
+    return jsonMock?.houses ?? []
+}
+
+extension HousesMock {
+    //expose this data to the rest of the app
+    static let houses = loadHouses()
+}
