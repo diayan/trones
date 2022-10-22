8889dda5315dc9c94d066b40a2c01d19aa1c0ee9
diayan <diayans@outlook.com>
2022-10-22 07:34:10 +0000

    Initial Commit

---
+++ Trones/TronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TronesApp.swift
+//  Trones
+//
+//  Created by diayan siat on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
