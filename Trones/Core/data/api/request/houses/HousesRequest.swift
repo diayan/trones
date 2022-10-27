5b11809ccc9225be0833062071241c5e192b5645
diayan <diayans@outlook.com>
2022-10-27 23:37:17 +0000

    fix minor issues

--- Trones/Core/data/api/request/houses/HousesRequest.swift
+++ Trones/Core/data/api/request/houses/HousesRequest.swift
@@ -9,11 +9,12 @@ import Foundation
 
 enum HousesRequest: RequestProtocol {
     case getHousesWith(page: Int, pageSize: Int?)
+    //case getHouseWith(name: String)
     
     var path: String {
         "/api/houses"
     }
-    
+        
     var urlParams: [String : String?] {
         switch self {
         case let .getHousesWith(page: page, pageSize: pageSize):
@@ -22,6 +23,9 @@ enum HousesRequest: RequestProtocol {
                 params["pageSize"] = String(pageSize)
             }
             return params
+//        case let .getHouseWith(name: name):
+//            var params = ["name": String(name)]
+//            return params
         }
     }
     
